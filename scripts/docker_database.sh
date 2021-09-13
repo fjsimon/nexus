87b4b9fc8cbb09a8699622fa044f37bc06ff013d
fjsimon <fjlopez.mail@gmail.com>
2021-09-13 13:47:45 +0100

    Added docker db script

--- scripts/docker_database.sh
+++ scripts/docker_database.sh
@@ -1,4 +1,5 @@
-
-
-# load db
+# Restore
 docker exec -i docker_container mysql -uuser -ppassword database_name < dump.sql
+
+# Backup
+docker exec docker_container /usr/bin/mysqldump -uroot -ppassword database_name > dump.sql
