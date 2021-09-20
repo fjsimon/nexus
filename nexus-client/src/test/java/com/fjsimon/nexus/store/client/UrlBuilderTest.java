2cc9d9a7a870ee4d68d1ca013395e7fcbe3f06be
fjsimon <fjlopez.mail@gmail.com>
2021-09-20 20:12:37 +0100

    Added url builder test

--- nexus-client/src/test/java/com/fjsimon/nexus/store/client/UrlBuilderTest.java
+++ nexus-client/src/test/java/com/fjsimon/nexus/store/client/UrlBuilderTest.java
@@ -2,7 +2,12 @@ package com.fjsimon.nexus.store.client;
 
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,7 +15,17 @@ import java.util.regex.Pattern;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@ExtendWith(MockitoExtension.class)
 public class UrlBuilderTest {
+
+    @InjectMocks
+    private UrlBuilder testee;
+
+    @BeforeEach
+    private void setup() {
+
+        ReflectionTestUtils.setField(testee, "baseUrl", "localhost");
+    }
 
     @Test
     public void regex_test() {
@@ -30,5 +45,18 @@ public class UrlBuilderTest {
         assertThat(matcher.find(), is(true));
         assertThat(matcher.group(1), is("format"));
 
+    }
+
+    @Test
+    public void link_builder_test() {
+
+        UrlBuilder.Spec spec = new UrlBuilder.Spec("#{baseUrl}/books?bibkeys=ISBN:#{isbn}&jscmd=#{jscmd}&format=#{format}");
+        UrlBuilder.LinkBuilder linkBuilder = testee.aLink(spec);
+        linkBuilder.with("isbn", "isbn");
+        linkBuilder.with("jscmd", "jscmd");
+        linkBuilder.with("format", "format");
+        linkBuilder.withParams("param1", "param1");
+
+        assertThat(linkBuilder.build(), is("localhost/books?bibkeys=ISBN:isbn&jscmd=jscmd&format=format&param1=param1"));
     }
 }
