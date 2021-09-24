168040755b5e506344d1c6d06257aed28d62f725
fjsimon <fjlopez.mail@gmail.com>
2021-09-24 19:43:28 +0100

    Added OpenLibraryServiceCallerTest

--- nexus-client/src/test/java/com/fjsimon/nexus/store/client/OpenLibraryServiceCallerTest.java
+++ nexus-client/src/test/java/com/fjsimon/nexus/store/client/OpenLibraryServiceCallerTest.java
@@ -1,8 +1,8 @@
 package com.fjsimon.nexus.store.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fjsimon.nexus.store.model.Book;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,8 +11,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
+import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
@@ -28,18 +30,36 @@ public class OpenLibraryServiceCallerTest {
     @InjectMocks
     private OpenLibraryServiceCaller testee;
 
-    private static final TypeReference<Map<String, Book>> BOOK_TYPE = new TypeReference<Map<String, Book>>() {};
+    private static final TypeReference<String> TYPE_REFERENCE = new TypeReference<String>() {};
 
     @Test
-    public void get_calls_delegates() {
+    public void get_calls_template_test() {
 
         ResponseEntity entity = new ResponseEntity<String>(HttpStatus.IM_USED);
 
         when(restTemplate.exchange(eq("url"), eq(HttpMethod.GET), (HttpEntity<?>) any(), (Class<Object>) any())).thenReturn(entity);
 
-        testee.get("url", BOOK_TYPE);
+        Optional<String> response = testee.get("url", TYPE_REFERENCE);
 
+        assertThat(response.isPresent(), is(false));
         verify(restTemplate, times(1)).exchange(eq("url"), eq(HttpMethod.GET), (HttpEntity<?>) any(), (Class<Object>) any());
+        verifyNoMoreInteractions(restTemplate, mapper);
     }
 
+    @Test
+    public void get_response_body_rest() throws JsonProcessingException {
+
+        String body = "body";
+        ResponseEntity entity = new ResponseEntity<>(body, HttpStatus.IM_USED);
+
+        when(restTemplate.exchange(eq("url"), eq(HttpMethod.GET), (HttpEntity<?>) any(), (Class<Object>) any())).thenReturn(entity);
+        when(mapper.readValue(eq("body"), eq(TYPE_REFERENCE))).thenReturn("parse");
+
+        Optional<String> response = testee.get("url", TYPE_REFERENCE);
+
+        assertThat(response.isPresent(), is(true));
+        verify(restTemplate, times(1)).exchange(eq("url"), eq(HttpMethod.GET), (HttpEntity<?>) any(), (Class<Object>) any());
+        verify(mapper, times(1)).readValue(eq("body"), eq(TYPE_REFERENCE));
+        verifyNoMoreInteractions(restTemplate, mapper);
+    }
 }
