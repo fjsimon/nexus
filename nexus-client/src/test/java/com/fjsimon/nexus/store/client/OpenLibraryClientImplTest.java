5913fb387ac6335755ddb15695198100f2f7300b
fjsimon <fjlopez.mail@gmail.com>
2021-09-26 19:19:15 +0100

    Added OpenLibraryServiceCallerTest

--- nexus-client/src/test/java/com/fjsimon/nexus/store/client/OpenLibraryClientImplTest.java
+++ nexus-client/src/test/java/com/fjsimon/nexus/store/client/OpenLibraryClientImplTest.java
@@ -6,8 +6,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OpenLibraryClientImplTest {
@@ -24,10 +28,14 @@ class OpenLibraryClientImplTest {
     @Test
     public void retrieveBook_test() {
 
-
         UrlBuilder.LinkBuilder linkBuilder = new UrlBuilder.LinkBuilder(new UrlBuilder.Spec("/books?bibkeys=ISBN:#{isbn}&jscmd=#{jscmd}&format=#{format}"));
         when(urlBuilder.aLink(any())).thenReturn(linkBuilder);
+        when(caller.get(anyString(), any())).thenReturn(Optional.of(Collections.EMPTY_LIST));
 
         testee.retrieveBook("isbn","jscmd","format");
+
+        verify(urlBuilder, times(1)).aLink(any());
+        verify(caller, times(1)).get(anyString(), any());
+        verifyNoMoreInteractions(urlBuilder, caller);
     }
 }
