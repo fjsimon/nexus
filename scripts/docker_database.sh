ddd50885ed59d0b7fadeb8288d0f3b2cfa05b388
fjsimon <fjlopez.mail@gmail.com>
2021-10-28 08:48:17 +0100

    fjsimon | update script

--- scripts/docker_database.sh
+++ scripts/docker_database.sh
@@ -3,3 +3,8 @@ docker exec -i docker_container mysql -uuser -ppassword database_name < dump.sql
 
 # Backup
 docker exec docker_container /usr/bin/mysqldump -uroot -ppassword database_name > dump.sql
+
+sudo service docker status
+sudo service docker start
+sudo service docker restart
+sudo service docker stop
