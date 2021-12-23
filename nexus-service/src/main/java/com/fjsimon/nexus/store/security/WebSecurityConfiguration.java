eeac66edcfeeb59f059112dcd378f6474ac408e4
fjsimon <fjlopez.mail@gmail.com>
2021-12-23 13:10:13 +0000

    fjsimon | modified entry points WebSecurityConfiguration.java

--- nexus-service/src/main/java/com/fjsimon/nexus/store/security/WebSecurityConfiguration.java
+++ nexus-service/src/main/java/com/fjsimon/nexus/store/security/WebSecurityConfiguration.java
@@ -3,13 +3,16 @@ package com.fjsimon.nexus.store.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -23,21 +26,19 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         // Entry points
-        http.authorizeRequests().anyRequest().permitAll();
-//                .antMatchers(HttpMethod.GET, "/", "/index.html", "/*.js", "/*.ico").permitAll()
-//                .antMatchers("/users/signin").permitAll()
-//                .anyRequest().authenticated();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/", "/index.html", "/*.js", "/*.ico", "/*.css").permitAll()
+                .antMatchers("/users/signin").permitAll()
+                .anyRequest().authenticated();
 
         // Disable CSRF (cross site request forgery)
         http.csrf().disable();
 
         http.headers().frameOptions().sameOrigin().disable();
 
-
-
         // No session will be created or used by spring security
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.addFilterBefore(new JwtTokenFilter(userDetailsService), UsernamePasswordAuthenticationFilter.class);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(new JwtTokenFilter(userDetailsService), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
