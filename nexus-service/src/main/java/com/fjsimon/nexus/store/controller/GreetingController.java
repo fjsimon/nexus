d6a7165ef1b5b3c48ef9962045c68c5d37d5fcb1
fjsimon <fjlopez.mail@gmail.com>
2022-01-20 08:45:26 +0000

    fjsimon | update GreetingController.java

--- nexus-service/src/main/java/com/fjsimon/nexus/store/controller/GreetingController.java
+++ nexus-service/src/main/java/com/fjsimon/nexus/store/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.fjsimon.nexus.store.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import com.fjsimon.nexus.store.model.Greeting;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +17,13 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
     @GetMapping("/login")
     public RedirectView redirectView(RedirectAttributes attributes) {
+
         return new RedirectView("/");
     }
 }
