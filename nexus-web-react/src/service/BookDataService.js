a8bcc4c282eb6aac1fb49d81f253232d86e65df3
fjsimon <fjlopez.mail@gmail.com>
2022-01-22 17:18:58 +0000

    fjsimon | update BookDataService.js

--- nexus-web-react/src/service/BookDataService.js
+++ nexus-web-react/src/service/BookDataService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 class BookDataService {
 
     retrieveBooks() {
-        return axios.get(`${process.env.API_URL}/books`);
+        return axios.get(`${process.env.API_URL}/books/scan`);
     }
 }
 
